cef22a586d73b888ede184dedfc220fadb195ada
“Poojanmodi” <modipoojan444@gmail.com>
2021-11-02 18:18:52 +0530

    Use Case 2 added successfully

--- src/com/employee/Employee.java
+++ src/com/employee/Employee.java
@@ -5,13 +5,16 @@ public class Employee {
 	public static void main(String[] args) {
 		
 		double attendance =Math.floor(Math.random()*2);
+		int wages;
 		
 		if(attendance == 1) {
-			System.out.println("Employee is Present");
+			wages=160;
+			System.out.println("Employee is Present\n Total Wage = " + wages);
 		}
 		
 		else {
-			System.out.println("Employee is absent");
+			wages=0;
+			System.out.println("Employee is absent\n Total Wage = " + wages);
 		}
 
 	}
