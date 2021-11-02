982b12a79d06d610090f712f4118c35a77fe0548
“Poojanmodi” <modipoojan444@gmail.com>
2021-11-02 22:45:26 +0530

    Use Case 6 added successfully

--- src/com/employee/Employee.java
+++ src/com/employee/Employee.java
@@ -4,26 +4,50 @@ public class Employee {
 
 	public static void main(String[] args) {
 		
-		int attendance =(int)Math.floor((Math.random()* 10) % 2);
-		double partTime =Math.floor((Math.random()* 10) % 2);
-		int wage;
-		switch (attendance) {
-		case 0:
-			System.out.println(" Employee is absent. ");
-			break;
-			
-		case 1: 
-			if (partTime != 0) {
-				wage = 160;
+		int wage = 0;
+		int days = 0, hours = 0;
+		int flag = 0;
+		while (days < 20 && hours < 100) {
+			int attendance = (int)Math.floor( (Math.random() * 10) % 2);
+			double partTime = Math.floor( (Math.random() * 10) % 2);
+			switch (attendance) {
+				case 0:
+					System.out.println(" Employee is absent. ");
+					break;
+					
+				case 1: 
+					if (partTime != 0) {
+						wage += 160;
+						hours += 8;
+						if (hours > 100) {
+							flag = 1;
+						}
+					}
+					else {
+						hours += 8;
+						wage += 160;
+						if (hours > 100) {
+							flag = 2;
+						}
+					}
+					days++;
+					System.out.println(" Employess is present");
+					System.out.println(" The wages earned = " + wage);
+					break;				
 			}
-			else {
-				wage = 160;
-			}
-			System.out.println(" Employess is present");
-			System.out.println(" The wages earned = " + wage);
-			break;
+		}
 		
+		if (flag == 1) {
+			hours -= 8;
+			days--;
+		}
+		else if (flag == 2) {
+			hours -= 8;
+			days--;
+		}
+		System.out.println(" Total working days = " + days);
+		System.out.println(" Total working hours = " + hours);
 	}
-}
 
 }
+
