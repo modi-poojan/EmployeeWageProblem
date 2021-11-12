df22ac732aae908eec9d850c21a5491917162c4e
modi-poojan <80687912+modi-poojan@users.noreply.github.com>
2021-11-12 23:08:17 +0530

    UseCase7 Refactored the code with Class Method

--- src/com/employee/Employee.java
+++ src/com/employee/Employee.java
@@ -2,17 +2,47 @@ package com.employee;
 
 public class Employee {
 
-	public static void main(String[] args) {
-		
-		double attendance =Math.floor(Math.random()*2);
-		
-		if(attendance == 1) {
-			System.out.println("Employee is Present");
+	public void calculate( int no_of_days, int max_hrs_in_month, int wages_per_hr){
+
+		int  is_part_time = 1;
+		int  is_full_time = 2;
+		int  part_time_hrs = 4;
+		int full_time_hrs = 8;
+		int wage = 0;
+		int days = 0;
+		int hours = 0;
+
+		while(days < no_of_days && hours < max_hrs_in_month){
+
+				int attendance = (int)Math.floor((Math.random)*10)%3);
+				switch(attendance){
+
+				case 0:
+						break;
+
+				case is_a_part_time:
+											if(hours + part_time_hrs >100){
+											break;
+											}
+											hours += part_time_hrs;
+											days++;
+											break;
+
+				case is_a_full_time:
+											if(hours + full_time_hrs >100){
+											break;
+											}
+											hours += full_time_hrs;
+											days++;
+											break;
+
+				}
+
 		}
-		
-		else {
-			System.out.println("Employee is absent");
-		}
+				wage = hours * wages_per_hr;
+				System.out.println(" Total wages earned = " + wage);
+				System.out.println(" Total working days = " + days);
+				System.out.println(" Total working hours = " + hours);
 
 	}
 
