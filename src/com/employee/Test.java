b8736d838069dbf93bccbaec8a543e7254070958
modi-poojan <80687912+modi-poojan@users.noreply.github.com>
2021-12-05 15:59:30 +0530

    UC8: Employee wage for multiple companies

--- src/com/employee/Test.java
+++ src/com/employee/Test.java
@@ -8,17 +8,27 @@ public class Test{
 
 	Employee obj = new Employee();
 	Scanner input = new Scanner(System.in);
-
-	System.out.print(" Please enter the max no of working days in a month: ");
+	
+	System.out.println("\nEmployee Wage Problem");
+	System.out.println("\nFor how many company do you want to calculate?");
+	int count = input.nextInt();
+	
+	for(int i = 1; i <= count; i++) {
+		
+	System.out.println("\nPlease enter company name");
+	String company = input.next();
+	
+	System.out.print(" \nPlease enter the max no of working days in a month: ");
 	int no_of_days = input.nextInt();
 
-	System.out.print(" Please enter the max no fo working hours in a month: ");
+	System.out.print(" \nPlease enter the max no fo working hours in a month: ");
 	int max_hrs_in_month = input.nextInt();
 
-	System.out.print(" Please enter the wages per hour: ");
+	System.out.print(" \nPlease enter the wages per hour: ");
 	int wages_per_hour = input.nextInt();
 
-	obj.calculate( no_of_days, max_hrs_in_month, wages_per_hour);
-}
-
+	obj.calculate( company ,no_of_days, max_hrs_in_month, wages_per_hour);
+	
+	}
+	}
 }
