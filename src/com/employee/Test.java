7ad7a8b2cd7b296169f1217b1632c44c05a3a57d
modi-poojan <80687912+modi-poojan@users.noreply.github.com>
2021-12-06 22:02:00 +0530

    UC9: Ability to save Total wages of Multiple Companies

--- src/com/employee/Test.java
+++ src/com/employee/Test.java
@@ -1,34 +1,47 @@
 package com.employee;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Test{
+public class Test {
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 
-	Employee obj = new Employee();
-	Scanner input = new Scanner(System.in);
-	
-	System.out.println("\nEmployee Wage Problem");
-	System.out.println("\nFor how many company do you want to calculate?");
-	int count = input.nextInt();
-	
-	for(int i = 1; i <= count; i++) {
+		EmpWageBuilder obj = new EmpWageBuilder();
+		Scanner input = new Scanner(System.in);
 		
-	System.out.println("\nPlease enter company name");
-	String company = input.next();
-	
-	System.out.print(" \nPlease enter the max no of working days in a month: ");
-	int no_of_days = input.nextInt();
+		ArrayList<CompanyEmpWage> temp;
+		temp = obj.getEmpCollection();
+		
+		System.out.println("\nEmployee Wage Problem");
+		System.out.println("\nFor how many company do you want to calculate?");
+		
+		int count = input.nextInt();
 
-	System.out.print(" \nPlease enter the max no fo working hours in a month: ");
-	int max_hrs_in_month = input.nextInt();
+		for (int i = 1; i <= count; i++) {
 
-	System.out.print(" \nPlease enter the wages per hour: ");
-	int wages_per_hour = input.nextInt();
+			System.out.println("\nPlease enter company name");
+			String company = input.next();
 
-	obj.calculate( company ,no_of_days, max_hrs_in_month, wages_per_hour);
-	
-	}
+			System.out.print(" \nPlease enter the max no of working days in a month: ");
+			int no_of_days = input.nextInt();
+
+			System.out.print(" \nPlease enter the max no of working hours in a month: ");
+			int max_hrs_in_month = input.nextInt();
+
+			System.out.print(" \nPlease enter the wages per hour: ");
+			int wages_per_hour = input.nextInt();
+
+			CompanyEmpWage call = new CompanyEmpWage(company);
+			obj.calculate(call, company, no_of_days, max_hrs_in_month, wages_per_hour);
+			temp.add(call);	
+		}
+		
+		obj.setEmpCollection(temp);
+
+		for (CompanyEmpWage i : obj.getEmpCollection()) {
+			System.out.println(i.toString());
+		}
+
 	}
 }
